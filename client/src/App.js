1c1efebaf066bf6412dd327fc48e9d255d2b2d31
Jason M. Goebel <jason.goebel@gmail.com>
2018-04-01 14:24:02 -0500

    client - server connection

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+ 
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('./api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+    console.log(body)
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{this.state.response}</p>
       </div>
     );
   }
