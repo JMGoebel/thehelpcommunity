5815aa565d2059b9bd060974ae377fc3513c5f5b
Jason M. Goebel <jason.goebel@gmail.com>
2018-04-03 20:01:19 -0500

    Basic CRUD functions in Express

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
-
+const ObjectID = require('mongodb').ObjectID
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,18 +11,40 @@ const dbName = 'thc-cluster';
 let conn;
 let db;
 
-app.get('/api/users', (req, res) => {
-  db.collection('users')
+app.use(express.json());
+
+// CREATE
+app.post('/api/create', (req, res) => {
+  db.collection(req.body.collection)
+    .insertOne(req.body.data)
+    .catch( err => console.log(err) )
+})
+
+// READ
+app.post('/api/read', (req, res) => {
+  db.collection(req.body.collection)
     .find({})
+    .filter(req.body.filter || {})
     .toArray()
     .then( users => res.send(users) )
     .catch( err => res.send(err) );
 })
 
-app.post('/api/users', (req, res) => {
-
+// UPDATE
+app.post('/api/update', (req, res) => {
+  db.collection(req.body.collection)
+    .updateOne(req.body.data, {$set: req.body.newData || req.body.data})
+    .catch( err => console.log(err) )
 })
 
+// DELETE
+app.post('/api/delete', (req, res) => {
+  db.collection(req.body.collection)
+    .deleteOne({"_id": ObjectID("5ac41fb8afebe9dc312c0f8a")})
+    .catch( err => console.log(err) )
+})
+
+// CONNECT TO DATABASE
 mongo.connect(url)
   .then( connection => {
     conn = connection;
